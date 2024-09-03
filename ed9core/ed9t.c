0d80e37f3bbc0a8c0c2a43486716c6524523c5ef
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-03 09:50:01 +0530

    initial code from chapter 1 of kilo tutorial

---
+++ ed9core/ed9t.c
@@ -0,0 +1,14 @@
+/**
+ * @file ed9t.c
+ * @author Abhishek Mishra (abhishekmishra3@gmail.com
+ * @brief Text editor based on kilo.c
+ * @version 0.1
+ * @date 2024-09-03
+ * 
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+int main() {
+  return 0;
+}
