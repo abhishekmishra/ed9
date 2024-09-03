dc9b07288adb6ed2634ed8d7876981d860931772
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-03 18:02:47 +0530

    write the row in the last line without crlf

--- ed9core/ed9t.c
+++ ed9core/ed9t.c
@@ -243,7 +243,13 @@ void editor_draw_rows()
 {
   for (int y = 0; y < E.screenrows; y++)
   {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+
+    /* write the crlf for all but the last line */
+    if (y < E.screenrows - 1)
+    {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
