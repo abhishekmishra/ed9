d9fc404bffd7c13e05d34cf47cb32a017a71b85f
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-03 13:32:27 +0530

    chapter 2 raw mode enabled.

--- ed9core/ed9t.c
+++ ed9core/ed9t.c
@@ -4,11 +4,98 @@
  * @brief Text editor based on kilo.c
  * @version 0.1
  * @date 2024-09-03
- * 
+ *
  * @copyright Copyright (c) 2024
  *
  */
 
-int main() {
+/*** INCLUDES ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** DATA ***/
+struct termios orig_termios;
+
+/*** TERMINAL ***/
+void die(const char *s)
+{
+  perror(s);
+  exit(1);
+}
+
+void disable_raw_mode()
+{
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  {
+    die("tcsetattr");
+  }
+}
+
+void enable_raw_mode()
+{
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  {
+    die("tcgetattr");
+  }
+
+  atexit(disable_raw_mode);
+
+  struct termios raw = orig_termios;
+
+  /* turn off ctrl-s and ctrl-q (IXON) */
+  /* turn off enter to newline (ICRNL) */
+  /* turn off miscellaneous flags*/
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+  /* turn off output processing (OPOST) */
+  raw.c_oflag &= ~(OPOST);
+
+  raw.c_cflag |= (CS8);
+
+  /* turn off echo (ECHO) and canonical mode (ICANON) */
+  /* turn off ctrl-z and ctrl-c (ISIG) */
+  /* turn off ctrl-v (IEXTEN) */
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  /* set timeout for read() */
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+  {
+    die("tcsetattr");
+  }
+}
+
+/*** INIT ***/
+
+int main()
+{
+  enable_raw_mode();
+
+  while (1)
+  {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+    {
+      die("read");
+    }
+
+    if (iscntrl(c))
+    {
+      printf("%d\r\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q')
+      break;
+  }
+
   return 0;
 }
