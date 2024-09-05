bb37d55a66b43247b1e8848ba99404d1ddc24d1e
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-05 08:14:15 +0530

    save-as file without a name

--- ed9core/ed9t.c
+++ ed9core/ed9t.c
@@ -92,6 +92,8 @@ EditorConfig E;
 
 /*** PROTOTYPES ***/
 void editor_set_status_message(const char *fmt, ...);
+void editor_refresh_screen();
+char *editor_prompt(char *prompt);
 
 /*** TERMINAL ***/
 void die(const char *s)
@@ -591,7 +593,12 @@ void editor_save()
 {
   if (E.filename == NULL)
   {
-    return;
+    E.filename = editor_prompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL)
+    {
+      editor_set_status_message("Save aborted");
+      return;
+    }
   }
   int len;
   char *buf = editor_rows_to_string(&len);
@@ -656,6 +663,49 @@ void ab_free(AppendBuffer *ab)
 }
 
 /*** INPUT ***/
+
+char *editor_prompt(char *prompt)
+{
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+  size_t buflen = 0;
+  buf[0] = '\0';
+  while (1)
+  {
+    editor_set_status_message(prompt, buf);
+    editor_refresh_screen();
+    int c = editor_read_key();
+    if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE)
+    {
+      if (buflen != 0)
+        buf[--buflen] = '\0';
+    }
+    else if (c == '\x1b')
+    {
+      editor_set_status_message("");
+      free(buf);
+      return NULL;
+    }
+    else if (c == '\r')
+    {
+      if (buflen != 0)
+      {
+        editor_set_status_message("");
+        return buf;
+      }
+    }
+    else if (!iscntrl(c) && c < 128)
+    {
+      if (buflen == bufsize - 1)
+      {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
 
 /**
  * @brief Move the cursor based on which key is pressed
